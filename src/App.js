17f9fa86a8d18e0c28382c671c3f03dcb729c1ed
Valerie Jane <valeriejanethoma@gmail.com>
2018-08-26 16:24:49 -0400

    add scroll component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { kitty } from './kitty';
+import Scroll from './Scroll';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      kitty: kitty,
+      kitty: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ kitty: users }));
   }
 
   onSearchChange = (event) =>  {
@@ -22,15 +29,18 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className='tc'>
-        <h1>Kitty Friends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList kitty={filteredKitties} />
-      </div>
-    );
+    if (this.state.kitty.length === 0) {
+      return <h1 className='tc'>Loading</h1>
+    } else {
+      return <div className="tc">
+          <h1 className="f1">Kitty Friends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList kitty={filteredKitties} />
+          </Scroll>
+        </div>;
+    }
   }
-  
 }
 
 export default App;
