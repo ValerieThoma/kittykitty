1036784cf67339a6f96869c0ae9b7048bdda97aa
Valerie Jane <valeriejanethoma@gmail.com>
2019-04-07 22:12:43 -0400

    Updates

---
+++ precache-manifest.7cbd2936e8abd9ecb14d748c86875b56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/kittykitty/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a06c511a21469d46e969",
+    "url": "/kittykitty/static/js/runtime~main.e1395f29.js"
+  },
+  {
+    "revision": "4b05ad68c39539ad2abb",
+    "url": "/kittykitty/static/js/main.9b5b0b0b.chunk.js"
+  },
+  {
+    "revision": "7ce1a0ca049b5f5256a2",
+    "url": "/kittykitty/static/js/2.f6b9c7de.chunk.js"
+  },
+  {
+    "revision": "4b05ad68c39539ad2abb",
+    "url": "/kittykitty/static/css/main.5e52edb1.chunk.css"
+  },
+  {
+    "revision": "7ce1a0ca049b5f5256a2",
+    "url": "/kittykitty/static/css/2.e424ee46.chunk.css"
+  },
+  {
+    "revision": "bb4b585648cd8b9edc2bb1730a910362",
+    "url": "/kittykitty/index.html"
+  }
+];
